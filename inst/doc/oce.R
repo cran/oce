cb76381470c68a76f579912a29585cfe43a99b07
Dan Kelley <Dan.Kelley@Dal.Ca>
2016-02-12 01:36:29 +0000

    version 0.9-18

--- inst/doc/oce.R
+++ inst/doc/oce.R
@@ -1,248 +1,130 @@
-### R code from vignette source 'oce.Rnw'
+## ---- echo = FALSE-------------------------------------------------------
+knitr::opts_chunk$set(collapse = TRUE, comment = "#>")
 
-###################################################
-### code chunk number 1: oce.Rnw:116-117
-###################################################
-options(keep.source=TRUE, width=60, prompt=' ', continue=' ')
+## ----eval=FALSE----------------------------------------------------------
+#  library(oce)
+#  d <- read.oce("station1.cnv")
+#  summary(d)
+#  plot(d)
 
-
-###################################################
-### code chunk number 2: oce.Rnw:129-132 (eval = FALSE)
-###################################################
-## library(oce)
-## d <- read.oce(filename)
-## plot(d)
-
-
-###################################################
-### code chunk number 3: ctdplot (eval = FALSE)
-###################################################
-## library(oce)
-## data(ctd)
-## plot(ctd)
-
-
-###################################################
-### code chunk number 4: ctdsummary (eval = FALSE)
-###################################################
-## summary(ctd)
-
-
-###################################################
-### code chunk number 5: ctdnames (eval = FALSE)
-###################################################
-## names(ctd@metadata)
-
-
-###################################################
-### code chunk number 6: ctdfig
-###################################################
+## ----fig.cap="**Figure 2.** An overview of a ctd dataset.", fig.width=6, fig.height=6, dpi=72, dev.args=list(pointsize=15), message=FALSE----
 library(oce)
 data(ctd)
+summary(ctd)
 plot(ctd)
 
-
-###################################################
-### code chunk number 7: ctdrawplot (eval = FALSE)
-###################################################
-## data(ctdRaw)
-## plotScan(ctdRaw)
-
-
-###################################################
-### code chunk number 8: ctdrawfig
-###################################################
+## ----fig.cap="**Figure 3.** Scanwise plot of the `ctdRaw` sample data set.  Note the spike at the start, the equilibration phase before the downcast, and the spurious freshening signal near the start of the upcast.", fig.width=5, fig.height=3, dpi=72, dev.args=list(pointsize=12)----
 data(ctdRaw)
 plotScan(ctdRaw)
 
+## ----eval=FALSE----------------------------------------------------------
+#  plotScan(ctdTrim(ctdRaw, "range",
+#                   parameters=list(item="scan", from=140, to=250)))
+#  plotScan(ctdTrim(ctdRaw, "range",
+#                   parameters=list(item="scan", from=150, to=250)))
 
-###################################################
-### code chunk number 9: ctdscaneg (eval = FALSE)
-###################################################
-## plotScan(ctdTrim(ctdRaw, "range",
-##                  parameters=list(item="scan", from=140, to=250)))
-## plotScan(ctdTrim(ctdRaw, "range",
-##                  parameters=list(item="scan", from=150, to=250)))
+## ----eval=FALSE----------------------------------------------------------
+#  ctdTrimmed <- ctdTrim(ctdRaw)
 
+## ----eval=FALSE----------------------------------------------------------
+#  plot(ctdDecimate(ctdTrim(read.ctd("stn123.cnv"))))
 
-###################################################
-### code chunk number 10: ctdtrimeg (eval = FALSE)
-###################################################
-## ctdTrimmed <- ctdTrim(ctdRaw)
+## ----eval=FALSE----------------------------------------------------------
+#  library(oce)
+#  # http://cchdo.ucsd.edu/data/7971/ar18_58JH19941029_ct1.zip
+#  # setwd("~/Downloads/ar18_58JH19941029_ct1")
+#  files <- system("ls *.csv", intern=TRUE)
+#  for (i in 1:length(files)) {
+#      x <- read.ctd(files[i])
+#      if (i == 1) {
+#          plotTS(x, Slim=c(31, 35.5), Tlim=c(-2, 10), type='o')
+#      } else {
+#          points(x[["salinity"]], x[["potential temperature"]])
+#          lines(x[["salinity"]], x[["potential temperature"]])
+#      }
+#  }
 
+## ----eval=FALSE----------------------------------------------------------
+#  data(section)
+#  plot(section, which=c(1, 2, 3, 99))
 
-###################################################
-### code chunk number 11: ctddectrim (eval = FALSE)
-###################################################
-## plot(ctdDecimate(ctdTrim(read.ctd("stn123.cnv"))))
-
-
-###################################################
-### code chunk number 12: ctdfix1 (eval = FALSE)
-###################################################
-## x <- read.ctd("nnsa_00934_00001_ct1.csv", type="WOCE")
-## x[["institute"]] <- "SIO" # better (using an accessor) but still bad
-
-
-###################################################
-### code chunk number 13: ctdfix2 (eval = FALSE)
-###################################################
-## x <- read.ctd("nnsa_00934_00001_ct1.csv", type="WOCE")
-## x <- oce.edit(x, "institute", "SIO") # better way
-
-
-###################################################
-### code chunk number 14: ctdfix3 (eval = FALSE)
-###################################################
-## x <- read.ctd("nnsa_00934_00001_ct1.csv", type="WOCE")
-## x <- oce.edit(x, "institute", "SIO", "human-parsed", "Dan Kelley")
-
-
-###################################################
-### code chunk number 15: arcticeg (eval = FALSE)
-###################################################
-## library(oce)
-## # Source: http://cchdo.ucsd.edu/data_access?ExpoCode=58JH199410
-## files <- system("ls *.csv", intern=TRUE)
-## for (i in 1:length(files)) {
-##     cat(files[i], "\n")
-##     x <- read.ctd(files[i])
-##     if (i == 1) {
-##         plotTS(x, xlim=c(31, 35.5), ylim=c(-1, 10), type="l", col="red")
-##     } else {
-##         lines(x[["salinity"]], x[["temperature"]], col="red")
-##     }
-## }
-
-
-###################################################
-### code chunk number 16: rangeg (eval = FALSE)
-###################################################
-## print(range(x[["temperature"]]))
-## print(range(x[["salinity"]]))
-
-
-###################################################
-### code chunk number 17: sectionplot (eval = FALSE)
-###################################################
-## data(section)
-## plot(section, which=c(1, 2, 3, 99))
-
-
-###################################################
-### code chunk number 18: oce.Rnw:454-455
-###################################################
-jpeg("section.jpg", quality=75, width=600, height=700, pointsize=12)
-
-
-###################################################
-### code chunk number 19: oce.Rnw:457-461
-###################################################
+## ----fig.cap="**Figure 4.** Portion of the CTD section designated A03, showing the Gulf Sream region.  The square on the cruise track corresponds to zero distance on the section.", fig.width=5, fig.height=5, dpi=72, dev.args=list(pointsize=12)----
+library(oce)
 data(section)
-GS <- subset(section, 102<=stationId&stationId<=124)
+GS <- subset(section, 102 <= stationId & stationId <= 124)
 GSg <- sectionGrid(GS, p=seq(0, 1600, 25))
 plot(GSg, which=c(1,99), map.xlim=c(-85,-(64+13/60)))
 
-
-###################################################
-### code chunk number 20: oce.Rnw:463-464
-###################################################
-dev.off()
-
-
-###################################################
-### code chunk number 21: oce.Rnw:503-504
-###################################################
-jpeg("topo.jpg", quality=70, width=800, height=400, pointsize=13)
-
-
-###################################################
-### code chunk number 22: oce.Rnw:506-509
-###################################################
+## ----fig.cap="**Figure 5.** World in Winkel Tripel projection, with the `section` profile sites indicated.", fig.width=4, fig.height=2.6, dpi=72----
 library(oce)
+data(coastlineWorld)
+par(mar=rep(0, 4))
+mapPlot(coastlineWorld, projection="+proj=wintri", col="lightgray")
+data(section)
+lon <- section[["longitude", "byStation"]]
+lat <- section[["latitude", "byStation"]]
+mapLines(lon, lat, col='red', cex=0.5)
+
+## ----echo=FALSE----------------------------------------------------------
+data(coastlineWorld)
+par(mar=rep(1, 4))
+
+## ----fig.keep="none"-----------------------------------------------------
+mapPlot(coastlineWorld, projection="+proj=moll") # Molleweide
+mapPlot(coastlineWorld, projection="+proj=eck4") # Eckert IV
+mapPlot(coastlineWorld, projection="+proj=robin") # Robinson
+
+## ----fig.cap="**Figure 6.** World bathymetry in Molleweide projection.", fig.width=5, fig.height=2.7, dpi=72, dev.args=list(pointsize=10)----
+par(mar=c(1.5, 1, 1.5, 1))
 data(topoWorld)
-plot(topoWorld, clatitude=30, clongitude=370, span=9000)
+topo <- decimate(topoWorld, 2) # coarsen grid: 4X faster plot
+lon <- topo[["longitude"]]
+lat <- topo[["latitude"]]
+z <- topo[["z"]]
+cm <- colormap(name="gmt_globe")
+drawPalette(colormap=cm)
+mapPlot(coastlineWorld, projection="+proj=moll", grid=FALSE)
+mapImage(lon, lat, z, colormap=cm)
 
+## ----fig.cap="**Figure 7.** North Atlantic in Lambert Conformal Conic projection.", fig.width=5, fig.height=3, dpi=72----
+par(mar=c(2, 2, 1, 1))
+lonlim <- c(-80, 0)
+latlim <- c(20, 60)
+mapPlot(coastlineWorld, projection="+proj=lcc +lat_1=30 +lat_2=50 +lon_0=-40",
+        longitudelim=lonlim, latitudelim=latlim)
 
-###################################################
-### code chunk number 23: oce.Rnw:511-512
-###################################################
-dev.off()
+## ---- fig.keep="none"----------------------------------------------------
+mapPlot(coastlineWorld, projection="+proj=merc",
+        longitudelim=lonlim, latitudelim=latlim)
+mapPlot(coastlineWorld, projection="+proj=aea +lat_1=30 +lat_2=70 +lon_0=-40",
+        longitudelim=lonlim, latitudelim=latlim)
 
+## ----fig.cap="**Figure 8.** Arctic coastlines in stereopolar projection.", fig.width=5, fig.height=5, dpi=72----
+par(mar=c(2, 2, 1, 1))
+mapPlot(coastlineWorld, projection="+proj=stere +lat_0=90",
+        longitudelim=c(-80,0), latitudelim=c(70, 110))
 
-###################################################
-### code chunk number 24: oce.Rnw:530-531
-###################################################
-jpeg("sealevel.jpg", quality=70, width=800, height=600, pointsize=20)
-
-
-###################################################
-### code chunk number 25: oce.Rnw:533-537
-###################################################
+## ----fig.cap="**Figure 9.** Sea-level timeseries measured in 2003 in Halifax Harbour.", fig.width=7, fig.height=5, dpi=72, dev.args=list(pointsize=16)----
 library(oce)
-#sealevel <- read.oce("../../tests/h275a96.dat")
 data(sealevel)
 plot(sealevel)
 
-
-###################################################
-### code chunk number 26: oce.Rnw:539-540
-###################################################
-dev.off()
-
-
-###################################################
-### code chunk number 27: oce.Rnw:617-618
-###################################################
-png("logger.png", width=600, height=300, pointsize=13)
-
-
-###################################################
-### code chunk number 28: oce.Rnw:620-623
-###################################################
-library(oce)
-data(logger)
-plot(logger, useSmoothScatter=TRUE)
-
-
-###################################################
-### code chunk number 29: oce.Rnw:625-626
-###################################################
-dev.off()
-
-
-###################################################
-### code chunk number 30: oce.Rnw:656-657
-###################################################
-png("adp.png", width=800, height=400, pointsize=18)
-
-
-###################################################
-### code chunk number 31: adpplot
-###################################################
+## ----fig.cap="**Figure 10.** Measurements made with a bottom-mounted ADP in the St Lawrence Estuary. The line near the surface indicates pressure measured by the ADP.", fig.width=5, fig.height=2, dpi=72----
 library(oce)
 data(adp)
-plot(adp, which=1, adorn=expression({lines(x[["time"]], x[["pressure"]])}))
+plot(adp, which=1)
+lines(adp[['time']], adp[['pressure']], lwd=2)
 
-
-###################################################
-### code chunk number 32: oce.Rnw:664-665
-###################################################
-dev.off()
-
-
-###################################################
-### code chunk number 33: oce.Rnw:690-694
-###################################################
+## ----fig.keep="none"-----------------------------------------------------
 library(oce)
 Sys.setenv(LANGUAGE="fr")
 data(ctd)
 plot(ctd)
 
+## ----echo=FALSE----------------------------------------------------------
+Sys.setenv(LANGUAGE="en")
 
-###################################################
-### code chunk number 34: oce.Rnw:755-761
-###################################################
+## ----fig.width=5, fig.height=5, fig.keep="none"--------------------------
 library(oce)
 swRho(34, 10, 100)
 swTheta(34, 10, 100)
@@ -250,104 +132,80 @@ swRho(34, swTheta(34, 10, 100), 0)
 swRho(34, swTheta(34, 10, 100, 200), 200)
 plotTS(as.ctd(c(30,40),c(-2,20),rep(0,2)), grid=TRUE, col="white")
 
-
-###################################################
-### code chunk number 35: oce.Rnw:766-771
-###################################################
+## ----fig.width=5, fig.height=5, fig.keep="none"--------------------------
 library(oce)
 data(ctd)
 pycnocline <- ctdTrim(ctd, "range",
                       parameters=list(item="pressure", from=5, to=12))
 plotProfile(pycnocline, which="density+N2")
 
-
-###################################################
-### code chunk number 36: oce.Rnw:775-779
-###################################################
+## ----fig.width=5, fig.height=5, fig.keep="none"--------------------------
 library(oce)
 data(ctd)
-pycnocline <- subset(ctd, 5<=pressure & pressure<=12)
+pycnocline <- subset(ctd, 5 <= pressure & pressure <= 12)
 plotProfile(pycnocline, which="density+N2")
 
+## ----eval=FALSE----------------------------------------------------------
+#  library(oce)
+#  # http://cchdo.ucsd.edu/data/7971/ar18_58JH19941029_ct1.zip
+#  # setwd("~/Downloads/ar18_58JH19941029_ct1")
+#  files <- system("ls *.csv", intern=TRUE)
+#  n <- length(files)
+#  ctds <- vector("list", n) # to hold the CTD objects
+#  station <- vector("list", n)
+#  for (i in 1:n) {
+#      ctds[[i]] <- read.ctd(files[i])
+#      station[[i]] <- ctds[[i]][["station"]]
+#  }
+#  S <- unlist(lapply(1:n, function(i) ctds[[i]][["salinity"]]))
+#  T <- unlist(lapply(1:n, function(i) ctds[[i]][["temperature"]]))
+#  p <- unlist(lapply(1:n, function(i) ctds[[i]][["pressure"]]))
+#  overall <- as.ctd(S, T, p)
+#  png("ar18_%02d.png")
+#  for (i in 1:n) {
+#      plotTS(overall, col='gray')
+#      lines(ctds[[i]][["salinity"]], ctds[[i]][["potential temperature"]])
+#      mtext(station[i], side=3, line=0)
+#  }
+#  dev.off()
 
-###################################################
-### code chunk number 37: oce.Rnw:785-789
-###################################################
+## ----fig.width=5, fig.height=5, fig.keep="none"--------------------------
 library(oce)
 data(section)
 ctd <- as.ctd(section[["salinity"]], section[["temperature"]], section[["pressure"]])
-plotTS(ctd)
+col <- ifelse(section[["longitude"]] > -30, "black", "gray")
+plotTS(ctd, col=col)
 
-
-###################################################
-### code chunk number 38: oce.Rnw:793-804
-###################################################
+## ----fig.width=5, fig.height=3, fig.keep="none"--------------------------
 library(oce)
 data(section)
-SS <- TT <- pp <- id <- NULL
-n <- length(section@data$station)
-for (stn in section@data$station) {
-    SS <- c(SS, stn[["salinity"]])
-    TT <- c(TT, stn[["temperature"]])
-    pp <- c(pp, stn[["pressure"]])
-}
-ctd <- as.ctd(SS, TT, pp)
-plotTS(ctd)
-
-
-###################################################
-### code chunk number 39: oce.Rnw:812-825
-###################################################
-library(oce)
-GS <- subset(section, 102<=stationId&stationId<=124)
+GS <- subset(section, 102 <= stationId & stationId <= 124)
 dh <- swDynamicHeight(GS)
-par(mfrow=c(2,1))
-plot(dh$distance, dh$height, type="b", xlab="", ylab="Dyn. Height [m]")
+par(mfrow=c(2,1), mar=c(3, 3, 1, 1), mgp=c(2, 0.7, 0))
+plot(dh$distance, dh$height, type="l", xlab="", ylab="Dyn. Height [m]")
 grid()
 # 1e3 metres per kilometre
-f <- coriolis(GS@data$station[[1]]@metadata$latitude)
-g <- gravity(GS@data$station[[1]]@metadata$latitude)
+latMean <- mean(GS[["latitude"]])
+f <- coriolis(latMean)
+g <- gravity(latMean)
 v <- diff(dh$height)/diff(dh$distance) * g / f / 1e3
-plot(dh$distance[-1], v, type="l", col="blue", xlab="Distance [km]", ylab="Velocity [m/s]")
+plot(dh$distance[-1], v, type="l", xlab="Distance [km]", ylab="Velocity [m/s]")
 grid()
-abline(h=0)
+abline(h=0, col='red')
 
-
-###################################################
-### code chunk number 40: oce.Rnw:833-839
-###################################################
+## ----fig.width=7, fig.height=3, fig.keep="none"--------------------------
 library(oce)
 data(sealevel)
 # Focus on 2003-Sep-28 to 29th, the time when Hurricane Juan caused flooding
 plot(sealevel,which=1,xlim=as.POSIXct(c("2003-09-24","2003-10-05"), tz="UTC"))
 abline(v=as.POSIXct("2003-09-29 04:00:00", tz="UTC"), col="red")
-mtext("Hurricane\nJuan", at=as.POSIXct("2003-09-29 04:00:00", tz="UTC"), col="red")
+mtext("Juan", at=as.POSIXct("2003-09-29 04:00:00", tz="UTC"), col="red")
 
-
-###################################################
-### code chunk number 41: oce.Rnw:845-847
-###################################################
+## ----results="hide", fig.keep="none"-------------------------------------
 library(oce)
 data(sealevel)
-
-
-###################################################
-### code chunk number 42: oce.Rnw:850-851
-###################################################
-elevation <- sealevel[["elevation"]]
-
-
-###################################################
-### code chunk number 43: oce.Rnw:855-858
-###################################################
-spectrum(elevation, spans=c(3,7))
-abline(v=1/12.42)
-mtext("M2",at=1/12.42,side=3)
-
-
-###################################################
-### code chunk number 44: oce.Rnw:876-877
-###################################################
-abline(v=as.POSIXct("2008-06-25 00:00:00"),col="red")
-
+m <- tidem(sealevel)
+oce.plot.ts(sealevel[['time']], sealevel[['elevation']] - predict(m),
+            ylab="Detided sealevel [m]", 
+            xlim=c(as.POSIXct("2003-09-20"), as.POSIXct("2003-10-08")))
 
